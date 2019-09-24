3862cf7296ed96f781123a99519e8617c70f7a80
Zhanna Kresteva <kresteva.astana@gmail.com>
2019-09-24 22:05:02 +0300

    UI with Navigation and Chartist

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Dashboard from './Components/Navigation/Dashboard.js';
 
 function App() {
   return (
     <div className="App">
-      <h1>PowerBox project</h1>
+      <Dashboard></Dashboard>
     </div>
   );
 }
