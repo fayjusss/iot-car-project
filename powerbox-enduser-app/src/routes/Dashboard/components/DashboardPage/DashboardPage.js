3c6e288db2f2b4b8815e2a2fb46573dff33d3546
Zhanna Kresteva <kresteva.astana@gmail.com>
2019-10-09 15:27:55 +0300

    Dashboard Elements added to Dashboard

--- powerbox-enduser-app/src/routes/Dashboard/components/DashboardPage/DashboardPage.js
+++ powerbox-enduser-app/src/routes/Dashboard/components/DashboardPage/DashboardPage.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Sidebar from 'containers/Sidebar'
 
 function DashboardPage({ dashboard, classes }) {
   return (
     <div className={classes.container}>
+      <Sidebar></Sidebar>
       <span>DashboardPage Component</span>
       <pre>{JSON.stringify(dashboard, null, 2)}</pre>
     </div>
