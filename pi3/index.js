d59bd33806c99fd6deda3485262dee7a667f62ca
Aram777 <aramabbasi@yahoo.com>
2019-10-09 21:37:48 +0300

    Adding blink function to trun the led on

--- pi3/index.js
+++ pi3/index.js
@@ -2,7 +2,27 @@ var fs = require('fs');
 var jwt = require('jsonwebtoken'); 
 var mqtt = require('mqtt'); 
 var BME280 = require('bme280-sensor');
- 
+// By Aram
+var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+var LED = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
+var blinkInterval = setInterval(blinkLED, 250); //run the blinkLED function every 250ms
+
+function blinkLED() { //function to start blinking
+  if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
+    LED.writeSync(1); //set pin state to 1 (turn LED on)
+  } else {
+    LED.writeSync(0); //set pin state to 0 (turn LED off)
+  }
+}
+
+function endBlink() { //function to stop blinking
+  clearInterval(blinkInterval); // Stop blink intervals
+  LED.writeSync(0); // Turn LED off
+  LED.unexport(); // Unexport GPIO to free resources
+}
+// END By Aram
+
+
 // The BME280 constructor options are optional.
 const options = {
   i2cBusNo   : 1, // defaults to 1
@@ -66,9 +86,11 @@ client.on('error', function(err) {
 }); 
  
 client.on('message', function(topic, message, packet) { 
+  setTimeout(endBlink, 5000);
   console.log(topic, 'message received: ', Buffer.from(message, 'base64').toString('ascii')); 
 }); 
  
+
 function createJwt(projectId, privateKeyFile, algorithm) { 
   var token = { 
     'iat': parseInt(Date.now() / 1000), 
