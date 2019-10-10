56209111b3aa558547237988f7d80d2fb34bcfb9
Fayjus Salehin <fsalehin@live.com>
2019-10-10 16:22:58 +0300

    Applied ESlint

--- powerbox-enduser-app/src/layouts/CoreLayout/CoreLayout.js
+++ powerbox-enduser-app/src/layouts/CoreLayout/CoreLayout.js
@@ -1,44 +1,43 @@
-import React, { useState }from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { Notifications } from 'modules/notification'
-import clsx from 'clsx';
-import { useTheme } from '@material-ui/styles';
-import { useMediaQuery } from '@material-ui/core';
+import clsx from 'clsx'
+import { useTheme } from '@material-ui/styles'
+import { useMediaQuery } from '@material-ui/core'
 import Navbar from 'containers/Navbar'
 import Sidebar from 'containers/Sidebar'
 
 function CoreLayout({ children, classes }) {
-  const theme = useTheme();
+  const theme = useTheme()
   const isDesktop = useMediaQuery(theme.breakpoints.up('lg'), {
     defaultMatches: true
-  });
-
-  const [openSidebar, setOpenSidebar] = useState(false);
+  })
+  const [openSidebar, setOpenSidebar] = useState(false)
 
   const handleSidebarOpen = () => {
-    setOpenSidebar(true);
-  };
+    setOpenSidebar(true)
+  }
 
   const handleSidebarClose = () => {
-    setOpenSidebar(false);
-  };
+    setOpenSidebar(false)
+  }
 
-  const shouldOpenSidebar = isDesktop ? true : openSidebar;
+  const shouldOpenSidebar = isDesktop ? true : openSidebar
 
   return (
     <div className={classes.container}>
-      <div className={clsx({
-        [classes.root]: true,
-        [classes.shiftContent]: isDesktop
-      })}>
-          <Navbar onSidebarOpen={handleSidebarOpen} />
-          <Sidebar
-            onClose={handleSidebarClose}
-            open={shouldOpenSidebar}
-            variant={isDesktop ? 'persistent' : 'temporary'}
-          ></Sidebar>
-          <div className={classes.children}>{children}</div>
-          <Notifications />
+      <div
+        className={clsx({
+          [classes.root]: true,
+          [classes.shiftContent]: isDesktop
+        })}>
+        <Navbar onSidebarOpen={handleSidebarOpen} />
+        <Sidebar
+          onClose={handleSidebarClose}
+          open={shouldOpenSidebar}
+          variant={isDesktop ? 'persistent' : 'temporary'}></Sidebar>
+        <div className={classes.children}>{children}</div>
+        <Notifications />
       </div>
     </div>
   )
