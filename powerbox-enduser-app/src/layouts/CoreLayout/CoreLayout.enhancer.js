6065d1cc981d05f819f79966bfeb94e85106feda
Fayjus Salehin <fsalehin@live.com>
2019-10-16 17:37:15 +0300

    Fixed sidebar

--- powerbox-enduser-app/src/layouts/CoreLayout/CoreLayout.enhancer.js
+++ powerbox-enduser-app/src/layouts/CoreLayout/CoreLayout.enhancer.js
@@ -1,4 +1,22 @@
+import { connect } from 'react-redux'
+import { withProps, compose, setDisplayName } from 'recompose'
+import { isEmpty, isLoaded } from 'react-redux-firebase'
+import { withRouter } from 'react-router-dom'
 import { withStyles } from '@material-ui/core/styles'
 import styles from './CoreLayout.styles'
 
-export default withStyles(styles)
+export default compose(
+  // Set component display name (more clear in dev/error tools)
+  setDisplayName('EnhancedCoreLayout'),
+  withRouter,
+  // Map redux state to props
+  connect(({ firebase: { auth } }) => ({
+    auth
+  })),
+  // Add custom props
+  withProps(({ auth }) => ({
+    authExists: isLoaded(auth) && !isEmpty(auth)
+  })),
+  // Add styles as classes prop
+  withStyles(styles)
+)
