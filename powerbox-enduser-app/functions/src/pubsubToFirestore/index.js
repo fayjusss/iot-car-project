160d0aafa43c2f5037b4b86c7381727f1c7e56e1
Fayjus Salehin <fsalehin@live.com>
2019-10-17 23:00:24 +0300

    Functions updated

--- powerbox-enduser-app/functions/src/pubsubToFirestore/index.js
+++ powerbox-enduser-app/functions/src/pubsubToFirestore/index.js
@@ -1,9 +1,24 @@
+const admin = require('firebase-admin')
 const functions = require('firebase-functions')
+const db = admin.firestore()
 
 export default functions.pubsub.topic('bme280').onPublish(message => {
-  const messageBody = message.data
-    ? Buffer.from(message.data, 'base64').toString()
-    : null
-  console.log(`Hello ${messageBody || 'World'}!`)
-  return null
+  const pubsubMessage = message.data
+  if (!pubsubMessage) {
+    throw new Error('No telemetry data was provided!')
+  }
+
+  const payload = Buffer.from(pubsubMessage, 'base64').toString()
+  const state = JSON.parse(payload)
+
+  db.doc(`powerboxes/${state.device_id}`)
+    .update({ state: state })
+    .then(writeResult => {
+      console.log({ result: 'Message with ID: ' + writeResult.id + ' added.' })
+      return
+    })
+    .catch(err => {
+      console.log(err)
+      return
+    })
 })
