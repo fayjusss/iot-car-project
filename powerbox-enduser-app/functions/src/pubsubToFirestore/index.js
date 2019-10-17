889a87cb1351de2ab915468a3a9712adf1cdb21e
Fayjus Salehin <fsalehin@live.com>
2019-10-17 19:20:14 +0300

    Added new cloud function

--- powerbox-enduser-app/functions/src/pubsubToFirestore/index.js
+++ powerbox-enduser-app/functions/src/pubsubToFirestore/index.js
@@ -1,7 +1,9 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
 
-exports.pubsubToFirestore= functions.pubsub.topic('bme280').onPublish((message) => {
-    const messageBody = message.data ? Buffer.from(message.data, 'base64').toString() : null;
-    console.log(`Hello ${messageBody || 'World'}!`);
-    return null;
-});
+export default functions.pubsub.topic('bme280').onPublish(message => {
+  const messageBody = message.data
+    ? Buffer.from(message.data, 'base64').toString()
+    : null
+  console.log(`Hello ${messageBody || 'World'}!`)
+  return null
+})
